1c92719153de3f8f65597f025f15ecb3ba9ee04c
xche529 <126313171+xche529@users.noreply.github.com>
2023-09-12 11:40:22 +1200

    Added new method
    feature/ added new method for movement control which can take an array of imageView as input

--- src/main/java/nz/ac/auckland/se206/MovementControl.java
+++ src/main/java/nz/ac/auckland/se206/MovementControl.java
@@ -2,25 +2,28 @@ package nz.ac.auckland.se206;
 
 import javafx.scene.image.ImageView;
 
-/** This method moves an image in the scene 
+/**
+ * This method moves an image in the scene
+ *
  * @param isToLeft - boolean value to determine if the image is moving to the left or right
  * @param timeToMove - the time in seconds that the image will move for
  * @param distanceToMove - the distance in pixels that the image will move
  * @param imageView - the image that will be moved
-*/
+ */
 public class MovementControl {
-  public static void moveToLeft(boolean isToLeft,int timeToMove, int distanceToMove, ImageView imageView) {
+  public static void moveToLeft(
+      boolean isToLeft, int timeToMove, int distanceToMove, ImageView imageView) {
     int timesToMove = timeToMove * 60;
     int distanceToMovePerIteration = distanceToMove / timesToMove;
     Thread timerThread =
         new Thread(
             () -> {
-              for(int i = 0; i < timesToMove; i++) {
+              for (int i = 0; i < timesToMove; i++) {
                 try {
-                  Thread.sleep(50/3);
-                  if (isToLeft == true){
-                  imageView.setX(imageView.getX() - distanceToMovePerIteration);
-                  }else{
+                  Thread.sleep(50 / 3);
+                  if (isToLeft == true) {
+                    imageView.setX(imageView.getX() - distanceToMovePerIteration);
+                  } else {
                     imageView.setX(imageView.getX() + distanceToMovePerIteration);
                   }
                 } catch (InterruptedException e) {
@@ -29,6 +32,29 @@ public class MovementControl {
               }
             });
     timerThread.start();
-    }
   }
 
+  public static void moveToLeft(
+      boolean isToLeft, int timeToMove, int distanceToMove, ImageView[] imageViews) {
+    int timesToMove = timeToMove * 60;
+    int distanceToMovePerIteration = distanceToMove / timesToMove;
+    Thread timerThread =
+        new Thread(
+            () -> {
+              for (int i = 0; i < timesToMove; i++) {
+                try {
+                  Thread.sleep(50 / 3);
+                  for (int j = 0; j < imageViews.length; j++)
+                    if (isToLeft == true) {
+                      imageViews[j].setX(imageViews[j].getX() - distanceToMovePerIteration);
+                    } else {
+                      imageViews[j].setX(imageViews[j].getX() + distanceToMovePerIteration);
+                    }
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              }
+            });
+    timerThread.start();
+  }
+}
