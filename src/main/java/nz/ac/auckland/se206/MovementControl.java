98a6f75d878f2c36e5047982535021c325e1b0cf
xche529 <126313171+xche529@users.noreply.github.com>
2023-09-12 01:22:02 +1200

    Added movement control class
    feature/ movement control class is add to control the movement of characters in the scene.

---
+++ src/main/java/nz/ac/auckland/se206/MovementControl.java
@@ -0,0 +1,29 @@
+package nz.ac.auckland.se206;
+
+import javafx.scene.image.ImageView;
+
+/** This method moves an image in the scene */
+public class MovementControl {
+  public static void moveToLeft(boolean isToLeft,int timeToMove, int distanceToMove, ImageView imageView) {
+    int timesToMove = timeToMove * 60;
+    int distanceToMovePerIteration = distanceToMove / timesToMove;
+    Thread timerThread =
+        new Thread(
+            () -> {
+              for(int i = 0; i < timesToMove; i++) {
+                try {
+                  Thread.sleep(50/3);
+                  if (isToLeft == true){
+                  imageView.setX(imageView.getX() + distanceToMovePerIteration);
+                  }else{
+                    imageView.setX(imageView.getX() - distanceToMovePerIteration);
+                  }
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+              }
+            });
+    timerThread.start();
+    }
+  }
+
