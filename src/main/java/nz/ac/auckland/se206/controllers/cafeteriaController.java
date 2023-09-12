d315315209cad2597b46952e6e29df0cf0fb5377
Joshua <jman988@aucklanduni.ac.nz>
2023-09-12 21:35:52 +1200

    added dummy functions for all clickable events to cafeteria controller class

--- src/main/java/nz/ac/auckland/se206/controllers/cafeteriaController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/cafeteriaController.java
@@ -2,6 +2,7 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
 public class cafeteriaController {
 
@@ -18,5 +19,63 @@ public class cafeteriaController {
     // TODO: set visability of all required items
   }
 
-  
+  /**
+   * This method is called when the user clicks on the vending machine.
+   *
+   * @throws ApiProxyException
+   */
+  @FXML
+  private void clickVendingMachine() {
+    // TODO: add code to handle click on vending machine
+  }
+
+  /**
+   * This method is called when the user clicks on the picture with a safe.
+   *
+   * @throws ApiProxyException
+   */
+  @FXML
+  private void clickPictureWithSafe() {
+    // TODO: add code to handle click on picture with safe
+  }
+
+  /**
+   * This method is called when the user clicks on the picture without a safe.
+   *
+   * @throws ApiProxyException
+   */
+  @FXML
+  private void clickPictureWithoutSafe() {
+    // TODO: add code to handle click on picture without safe
+  }
+
+  @FXML
+  private void vendingMachineMouseEntered() {
+    // TODO
+  }
+
+  @FXML
+  private void vendingMachineMouseExited() {
+    // TODO
+  }
+
+  @FXML
+  private void pictureWithSafeMouseEntered() {
+    // TODO
+  }
+
+  @FXML
+  private void pictureWithSafeMouseExited() {
+    // TODO
+  }
+
+  @FXML
+  private void pictureWithoutSafeMouseEntered() {
+    // TODO
+  }
+
+  @FXML
+  private void pictureWithoutSafeMouseExited() {
+    // TODO
+  }
 }
