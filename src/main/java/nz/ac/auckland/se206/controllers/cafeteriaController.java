2fd1175bc7f06681cee5b0742d950ebabb055b0c
Joshua <jman988@aucklanduni.ac.nz>
2023-09-12 21:23:03 +1200

    added all the objects to controller class and set up initialize method

--- src/main/java/nz/ac/auckland/se206/controllers/cafeteriaController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/cafeteriaController.java
@@ -1,5 +1,22 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+
 public class cafeteriaController {
-    
+
+  @FXML private Rectangle pictureWithSafe;
+  @FXML private Rectangle pictureWithoutSafe;
+  @FXML private Rectangle vendingMachine;
+
+  /**
+   * Initializes the cafeteria view, it is called when the room loads.
+   *
+   * @throws ApiProxyException
+   */
+  private void initialize() {
+    // TODO: set visability of all required items
+  }
+
+  
 }
