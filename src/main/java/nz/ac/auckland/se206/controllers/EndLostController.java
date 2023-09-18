852795d1f90fd167e9c27fd96944cea975a445d0
Joshua <jman988@aucklanduni.ac.nz>
2023-09-19 00:16:33 +1200

    fixed bug in method declaration, can now navigate to start interface from loss screen

--- src/main/java/nz/ac/auckland/se206/controllers/EndLostController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndLostController.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se206.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
@@ -10,9 +11,10 @@ public class EndLostController {
 
     @FXML private Button restartButton;
 
-    void goToStart() {
+    @FXML
+    private void onTryAgain(MouseEvent event) {
         Scene scene = restartButton.getScene();
-        scene.setRoot(SceneManager.getUiRoot(AppUi.CAFETERIA));
+        scene.setRoot(SceneManager.getUiRoot(AppUi.START_INTERFACE));
     }
     
 }
