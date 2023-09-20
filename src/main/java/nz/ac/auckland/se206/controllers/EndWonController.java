76e7590f285cac6828a058b54ce4ff1a5053e03b
Joshua <jman988@aucklanduni.ac.nz>
2023-09-20 13:38:42 +1200

    redesigned end won sceen and added a button to return to start

--- src/main/java/nz/ac/auckland/se206/controllers/EndWonController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndWonController.java
@@ -1,9 +1,20 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import nz.ac.auckland.se206.SceneManager;
+import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class EndWonController {
 
   @FXML private Label resultLabel;
+  @FXML private Button restartButton;
+
+  @FXML
+  private void goToStart() {
+    Scene scene = restartButton.getScene();
+    scene.setRoot(SceneManager.getUiRoot(AppUi.START_INTERFACE));
+  }
 }
