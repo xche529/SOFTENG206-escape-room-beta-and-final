185fb989ae30687aedde1606493576e89d2c2d93
xche529 <126313171+xche529@users.noreply.github.com>
2023-09-20 20:29:12 +1200

    Added code to fit the screen size

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.transform.Scale;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.controllers.CafeteriaController;
@@ -13,10 +15,12 @@ import nz.ac.auckland.se206.controllers.OfficeController;
 import nz.ac.auckland.se206.controllers.StartInterfaceController;
 
 /**
- * This is the entry point of the JavaFX application, while you can change this class, it should
+ * This is the entry point of the JavaFX application, while you can change this
+ * class, it should
  * remain as the class that runs the JavaFX application.
  */
 public class App extends Application {
+  public static Scale scale;
 
   private static Scene scene;
 
@@ -29,7 +33,8 @@ public class App extends Application {
   }
 
   /**
-   * Returns the node associated to the input file. The method expects that the file is located in
+   * Returns the node associated to the input file. The method expects that the
+   * file is located in
    * "src/main/resources/fxml".
    *
    * @param fxml The name of the FXML file (without extension).
@@ -41,14 +46,28 @@ public class App extends Application {
   }
 
   /**
-   * This method is invoked when the application starts. It loads and shows the "Canvas" scene.
+   * This method is invoked when the application starts. It loads and shows the
+   * "Canvas" scene.
    *
    * @param stage The primary stage of the application.
    * @throws IOException If "src/main/resources/fxml/canvas.fxml" is not found.
    */
   @Override
   public void start(final Stage stage) throws IOException {
-
+    double overallScale = 1;
+    Screen screen = Screen.getPrimary();
+    double width = screen.getBounds().getWidth();
+    double height = screen.getBounds().getHeight();
+    double ratio = width / height;
+    if (ratio < 1113.0 / 800.0) {
+      overallScale = (width / 1113.0);
+      System.out.println("The game start with scale:" + overallScale);
+    } else {
+      overallScale = (height / 800.0);
+      System.out.println("The game start with scale:" + overallScale);
+    }
+    overallScale = overallScale * 0.98;
+    scale = new Scale(overallScale, overallScale);
     FXMLLoader endScreenWonLoader = loadFxml("endScreenWon");
     FXMLLoader endScreenLostLoader = loadFxml("endScreenLost");
     FXMLLoader officeSceneLoader = loadFxml("officeScene");
@@ -60,6 +79,11 @@ public class App extends Application {
     SceneManager.addUi(AppUi.OFFICE, officeSceneLoader.load());
     SceneManager.addUi(AppUi.CAFETERIA, cafeteriaLoader.load());
     SceneManager.addUi(AppUi.START_INTERFACE, StartInterfaceLoader.load());
+    SceneManager.getUiRoot(AppUi.CAFETERIA).getTransforms().add(scale);
+    SceneManager.getUiRoot(AppUi.OFFICE).getTransforms().add(scale);
+    SceneManager.getUiRoot(AppUi.START_INTERFACE).getTransforms().add(scale);
+    SceneManager.getUiRoot(AppUi.END_WON).getTransforms().add(scale);
+    SceneManager.getUiRoot(AppUi.END_LOST).getTransforms().add(scale);
 
     CafeteriaController cafeteriaController = cafeteriaLoader.getController();
     OfficeController officeController = officeSceneLoader.getController();
@@ -74,13 +98,12 @@ public class App extends Application {
 
     Safe.getRandomCode();
 
-    scene = new Scene(SceneManager.getUiRoot(AppUi.START_INTERFACE), 1113, 800);
-
+    scene = new Scene(SceneManager.getUiRoot(AppUi.START_INTERFACE), 1113 * overallScale, 800 * overallScale);
     stage.setScene(scene);
     scene.addEventFilter(javafx.scene.input.KeyEvent.KEY_PRESSED, event -> {
       if (event.getCode() == KeyCode.LEFT) {
         scene.setRoot(SceneManager.switchRoom(true));
-      }else if(event.getCode() == KeyCode.RIGHT) {
+      } else if (event.getCode() == KeyCode.RIGHT) {
         scene.setRoot(SceneManager.switchRoom(false));
       }
     });
