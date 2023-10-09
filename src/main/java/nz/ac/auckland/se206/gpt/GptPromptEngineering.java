3e477425a13fde5d8e12271d2dc0d88a1eabe8e0
xche529 <xche529@aucklanduni.ac.nz>
2023-10-09 22:59:47 +1300

    Optimized prompt for guard

--- src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
+++ src/main/java/nz/ac/auckland/se206/gpt/GptPromptEngineering.java
@@ -40,114 +40,41 @@ public class GptPromptEngineering {
   public static String getGuardSetUp(String wordToGuess) {
     if (GameState.difficulty == Difficulty.HARD) {
       // get the guards story if the difficulty is hard
-      return "(You are playing the role of a guard who is helping someone escape an escape room"
-          + " where they are the prisoner.  You cannot give the prisoner any hints as it is"
-          + " too risky and others might get suspicious. You should give the prisoner a"
-          + " riddle with the answer "
+      return "(You are a guard who is helping a prisoner escape an prison You should give the"
+                 + " prisoner a riddle with the answer of:"
           // append riddle answer
           + wordToGuess
-          + " to give them their first clue in your first message. The riddle can go up to 3 or 4"
-          + " lines. "
-          // confirm that the only answer is the word to guess
-          + wordToGuess
-          + " is the only correct answer. You should tell them that the answer to the riddle will"
-          + " help them escape in your first message. Never say the word "
-          + wordToGuess
-          + ". Keep your messages as concise as possible. Never write from any perspective but your"
-          + " own. Do not write anything from the point of view of the prisoner. Only write what"
-          + " the guard says. Never refer to the prison as an escape room. When the Riddle is"
-          + " guessed correctly return only 'Correct'. Messages should start with Guard: every"
-          + " time. Never give the prisoner help if they get the riddle wrong. Only tell them their"
-          + " answer is wrong and to try again.\r\n"
-          + ")";
+          + " in your first message. The riddle can go up to 3 or 4 lines. and easy to answer You"
+          + " cannot, no matter what, reveal the answer even if the player asks for it.Even if"
+          + " player gives up, do not give the answer. Do not give any hints. Keep your messages as"
+          + " concise as possible. Do not write anything from the point of view of the prisoner."
+          + " Only write what the guard says. When the Riddle is guessed correctly return only"
+          + " 'Correct')";
 
     } else if (GameState.difficulty == Difficulty.MEDIUM) {
       // get the guards story if the difficulty is medium
-      return "(You are playing the role of a guard who is helping someone escape an escape room"
-                 + " where they are the prisoner.  You can give the prisoner five hint. put (HINT)"
-                 + " at the end of your response if you are giving a hint You should give the"
-                 + " prisoner a riddle with the answer and one hint at a time, don't give hint if"
-                 + " not asked."
+      return "(You are a guard who is helping a prisoner escape an prison You should give the"
+                 + " prisoner a riddle with the answer of:"
+          // append riddle answer
           + wordToGuess
-          + " to give them their first clue in your first message. The riddle "
-          + "can go up to 3 or 4 lines. "
-          + wordToGuess
-          + " is the only correct answer. You should tell them that the answer to the riddle will"
-          + " help them escape in your first message as well as giving them the riddle. Never say"
-          + " the word "
-          + wordToGuess
-          // keep messages concise
-          + ". Keep your messages as concise as possible. Never write from any perspective but your"
-          + " own. Do not write anything from the point of view of the prisoner. Only write what"
-          + " the guard says. Write in 1st person only. Never refer to the prison as an escape"
-          + " room. When the Riddle is guessed correctly return only 'Correct'. Messages should"
-          + " never start with 'Guard: '. Never give the prisoner help if they get the riddle"
-          + " wrong. Only tell them their answer is wrong and to try again. Each message you"
-          + " receive will have a phrase on the end to indicate what stages of the game the player"
-          + " is at. The stages are in order as follows: riddle unsolved, riddle solved, word"
-          + " found, cypher found,  safe found and  safe unlocked. Never repeat these stages in"
-          + " your own messages. You should never say any of the phrases used as indicators. Do not"
-          + " give any indication to the player that you are getting phrases on the end of the"
-          + " message. Do not give out the hints unless you are asked for them by the prisoner. The"
-          + " hints you can give out are as follows: for riddle unsolved give clues to the riddle,"
-          + " for riddle solved hint that the player should check the "
-          // suggesting hints to give player
-          + wordToGuess
-          + ", for word found suggest that there might be some sort of code, if asked for a second"
-          + " clue suggest that there might be some sort of cypher in the wardens office. For"
-          + " cypher found suggest that there must be somewhere to put in a code, if asked again"
-          + " suggest maybe one of the other prisoners knows something. For safe found suggest that"
-          + " the cypher and word must link to the safe. a second clue could be that each letter in"
-          + " the word must corelate to a number. For safe unlocked suggest that there must be a"
-          + " phone we can call the number from. Remember you can give out a maximum of five"
-          // defining rules about hints
-          + " hints. Asking what to do next or for a clue counts as a hint. Any information you"
-          + " give out about rooms that is asked for is a hint. Never give the same hint twice. DO"
-          + " NOT give answer at any time. put (HINT) at the end of your response if you are giving"
-          + " a hint!!!\r\n"
-          + ")";
+          + " in your first message. The riddle can go up to 3 or 4 lines. and easy to answer You"
+          + " can only give the prisoner 5 hints, after that no more hints, when you give hint,put"
+          + " (HINT) at the end of your response. Do not reveal the answer even if the player asks"
+          + " for it.Even if player gives up, do not give the answer. Keep your messages as concise"
+          + " as possible. Do not write anything from the point of view of the prisoner. Only write"
+          + " what the guard says. When the Riddle is guessed correctly return only 'Correct')";
     }
     // get the guards story if the difficulty is easy
-    return "(You are playing the role of a guard who is helping someone escape an escape room where"
-        + " they are the prisoner.  You can give the prisoner infinite"
-        // set unlimited hints
-        + " hints. You should give the prisoner a riddle with "
-        + "the answer "
+    return "(You are a guard who is helping a prisoner escape an prison You should give the"
+               + " prisoner a riddle with the answer of:"
+        // append riddle answer
         + wordToGuess
-        // give a few lines for the riddles
-        + " to give them their first clue in your first message. The riddle "
-        + "can go up to 3 or 4 lines. "
-        + wordToGuess
-        + " is the only correct answer. You should tell them that the answer to the riddle will"
-        + " help them escape in your first message as well as giving them the riddle. Never say the"
-        + " word "
-        // keep message concise as possible
-        + wordToGuess
-        + ". Keep your messages as concise as possible. Never write from any perspective but your"
-        + " own. Do not write anything from the point of view of the prisoner. Only write what the"
-        + " guard says. Write in 1st person only. Never refer to the prison as an escape room. When"
-        + " the Riddle is guessed correctly return only 'Correct'. Messages should never start with"
-        + " 'Guard: '. Never give the prisoner help if they get the riddle wrong. Only tell them"
-        + " their answer is wrong and to try again. Each message you receive will have a phrase on"
-        + " the end to indicate what stages of the game the player is at. The stages are in order"
-        + " as follows: riddle unsolved, riddle solved, word found, cypher found,  safe found and "
-        + " safe unlocked. Never repeat these stages in your own messages. You should never say any"
-        + " of the phrases used as indicators. Do not give any indication to the player that you"
-        + " are getting phrases on the end of the message. Do not give out the hints unless you are"
-        + " asked for them by the prisoner. The hints you can give out are as follows: for riddle"
-        + " unsolved give clues to the riddle, for riddle solved hint that the player should check"
-        + " the "
-        + wordToGuess
-        // defining some rules about hints
-        + ", for word found suggest that there might be some sort of code, if asked for a second"
-        + " clue suggest that there might be some sort of cypher in the wardens office. For cypher"
-        + " found suggest that there must be somewhere to put in a code, if asked again suggest"
-        + " maybe one of the other prisoners knows something. For safe found suggest that the"
-        + " cypher and word must link to the safe. a second clue could be that each letter in the"
-        + " word must corelate to a number. For safe unlocked suggest that there must be a phone we"
-        + " can call the number from. Any information you give out about rooms that is asked for is"
-        + " a hint. Never give the same hint twice. riddle unsolved.\r\n"
-        + ")";
+        + " in your first message. The riddle can go up to 3 or 4 lines. and easy to answer You can"
+        + " only give the prisoner as much hint as you want but You cannot, no matter what, reveal"
+        + " the answer even if the player asks for it.Even if player gives up, do not give the"
+        + " answer. Keep your messages as concise as possible. Do not write anything from the point"
+        + " of view of the prisoner. Only write what the guard says. When the Riddle is guessed"
+        + " correctly return only 'Correct')";
   }
 
   public static String getPrisonerOneSetUp() {
