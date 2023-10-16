604520c6721c066d366d4ff69a80d5a7fb03d269
manners99 <126521915+manners99@users.noreply.github.com>
2023-10-17 01:57:28 +1300

    added javadocs and comments for PlayHistory and SFX

--- src/main/java/nz/ac/auckland/se206/SFX.java
+++ src/main/java/nz/ac/auckland/se206/SFX.java
@@ -14,12 +14,12 @@ public class SFX {
     this.path = path;
   }
 
+  /** Plays the sound effect */
   public void playSFX() {
-
-    // runs the safe opening sound effect
     Media media = new Media(new File(path).toURI().toString());
     MediaPlayer mediaPlayer = new MediaPlayer(media);
     mediaPlayer.setVolume(GameState.sfxVolume);
+    // plays the sound effect in a separate thread to avoid lag
     Task<Void> task =
         new Task<Void>() {
           @Override
@@ -27,29 +27,34 @@ public class SFX {
             mediaPlayer.play();
             pauseSafeNoise(mediaPlayer);
             return null;
-        }
-    };
+          }
+        };
     Thread thread = new Thread(task);
     thread.start();
-}
+  }
 
-private void pauseSafeNoise(MediaPlayer mediaPlayer) throws InterruptedException {
-      if (path == "src/main/resources/sounds/door-opening-and-closing-18398.mp3") {
-        //plays the first 3 seconds then pauses the sound effect
-        Thread.sleep(3000);
-        mediaPlayer.pause();
-        Duration length = mediaPlayer.getCurrentTime();
-        // animates all the arrows if the riddle is resolved
-        GameState.isSafeClosed()
-            .addListener(
-                (observable, oldValue, newValue) -> {
-                  if (newValue) {
-                    mediaPlayer.seek(length);
-                    mediaPlayer.play();
-                    GameState.isSafeClosed().set(false);
-                  }
-                });
-      }
-
+  /**
+   * Pauses the safe opening sound effect after 3 seconds and resumes it when the safe is closed
+   *
+   * @param mediaPlayer the media player that plays the sound effect
+   * @throws InterruptedException
+   */
+  private void pauseSafeNoise(MediaPlayer mediaPlayer) throws InterruptedException {
+    if (path == "src/main/resources/sounds/door-opening-and-closing-18398.mp3") {
+      // plays the first 3 seconds then pauses the sound effect
+      Thread.sleep(3000);
+      mediaPlayer.pause();
+      Duration length = mediaPlayer.getCurrentTime();
+      // Checks if the safe is closed and resumes the sound effect
+      GameState.isSafeClosed()
+          .addListener(
+              (observable, oldValue, newValue) -> {
+                if (newValue) {
+                  mediaPlayer.seek(length);
+                  mediaPlayer.play();
+                  GameState.isSafeClosed().set(false);
+                }
+              });
+    }
   }
 }
